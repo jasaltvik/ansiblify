f8b7d7003f35df94639918ca5c8c2e7886527c70
Jonas Arneberg <jonas.arneberg@itslearning.com>
2016-01-18 13:58:27 +0100

    Make sure pip and Ansible are installed with the init script
    First iteration of the setup script that will be run before the
    Ansible playbooks(s).
    
    'make setup' should now install pip, and then latest Ansible

--- initial_setup.sh
+++ initial_setup.sh
@@ -5,4 +5,30 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-printf "\e[34mInstalling stuff!\e[0m\n"
+printf "\e[30;42mInstalling pip!\e[0m\n"
+printf "\e[30;42mThis install follows the official install guide from https://pip.pypa.io/en/stable/installing/\e[0m\n\n"
+
+printf "\e[30;42mFetching official pip bootstrap script\e[0m\n"
+wget https://bootstrap.pypa.io/get-pip.py
+printf '\e[30;42mDone\e[0m\n\n'
+
+printf "\e[30;42mRun pip bootstrap script\e[0m\n"
+sudo -H python get-pip.py
+printf '\e[30;42mDone\e[0m\n\n'
+
+printf "\e[30;42mpip version information\e[0m\n"
+pip --version
+printf '\e[30;42mDone\e[0m\n\n'
+
+printf "\e[30;42mRemove downloaded pip script\e[0m\n"
+rm get-pip.py
+printf '\e[30;42mDone\e[0m\n\n'
+
+printf "\e[30;42mInstalling latest Ansible via pip!\e[0m\n"
+sudo -H pip install ansible
+printf '\e[30;42mDone\e[0m\n\n'
+
+printf "\e[30;42mAnsible version information\e[0m\n"
+ansible --version
+printf '\e[30;42mDone\e[0m\n\n'
+
